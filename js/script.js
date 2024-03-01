03fc51531ee90764d3f8ae1fca940fc0444832d9
calculator12321 <161859364+calculator12321@users.noreply.github.com>
2024-03-01 21:36:58 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,57 +1,57 @@
-const screan = document.querySelector('.screan');
-const btns = document.querySelectorAll('.button');
-const znak = document.querySelector('.button-znak')
+document.addEventListener('DOMContentLoaded', () => {
+    const screan = document.querySelector('.screan');
+    const btns = document.querySelectorAll('.button');
+    const znak = document.querySelector('.button-znak')
 
-btns.forEach((element) => {
-    element.addEventListener('click', () => {
-        if (screan.textContent === '0') {
-            screan.textContent = '';
-            screan.textContent += element.textContent;
-        } 
-        else {
-            screan.textContent += element.textContent;
+    btns.forEach((element) => {
+        element.addEventListener('click', () => {
+            if (screan.textContent === '0') {
+                screan.textContent = '';
+                screan.textContent += element.textContent;
+            } 
+            else {
+                screan.textContent += element.textContent;
+            }
+        })
+    })
+
+    znak.addEventListener('click', () => {
+        let result1 = [];
+        let result2 = [];
+        let znak = [];
+        let index1;
+        let arr = screan.textContent.split('');
+        arr.forEach((element, index) => {
+            if (!isNaN(element) && index1 > 0){
+                result2.push(element);
+            }
+            else if (isNaN(element)){
+                znak.push(element);
+                index1 = index;
+            }
+            else
+                result1.push(element);
+        });
+        result1 = +(result1.join(''));
+        result2 = +(result2.join(''));
+        znak = znak.join('');
+        switch(znak) {
+            case '+':
+                screan.textContent = result1+result2
+                break;
+            case '-':
+                screan.textContent = result1 - result2
+                break;
+            case '*':
+                screan.textContent = result1 * result2
+                break;
+            case '/':
+                screan.textContent = result1 / result2
+                break;
         }
     })
+    console.dir(navigator)
 })
-
-znak.addEventListener('click', () => {
-    let result1 = [];
-    let result2 = [];
-    let znak = [];
-    let index1;
-    let arr = screan.textContent.split('');
-    arr.forEach((element, index) => {
-        if (!isNaN(element) && index1 > 0){
-            result2.push(element);
-        }
-        else if (isNaN(element)){
-            znak.push(element);
-            index1 = index;
-        }
-        else
-            result1.push(element);
-    });
-    result1 = +(result1.join(''));
-    result2 = +(result2.join(''));
-    znak = znak.join('');
-    switch(znak) {
-        case '+':
-            screan.textContent = result1+result2
-            break;
-        case '-':
-            screan.textContent = result1 - result2
-            break;
-        case '*':
-            screan.textContent = result1 * result2
-            break;
-        case '/':
-            screan.textContent = result1 / result2
-            break;
-    }
-})
-
-// + znak.join('') + +(result2.join(''))
-
 
 
 
